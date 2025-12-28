85f9401851785721b1620a573f54e81e207ef784
Pat <patrick.bobovnik@hhu.de>
2025-12-28 17:52:30 +0100

    Interface Changes

--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,74 @@
 import { WordleSolver } from "./solver";
 
  let solver = new WordleSolver();
+ let currentRow: number = 0; //Tracks in which row words should be inserted in
 
-const wordleList = document.querySelector(".js-wordle-list") as HTMLDivElement;
-const letter1Box = document.querySelector(".js-letter-box-1") as HTMLDivElement;
-const letter2Box = document.querySelector(".js-letter-box-2") as HTMLDivElement;
-const letter3Box = document.querySelector(".js-letter-box-3") as HTMLDivElement;
-const letter4Box = document.querySelector(".js-letter-box-4") as HTMLDivElement;
-const letter5Box = document.querySelector(".js-letter-box-5") as HTMLDivElement;
+
+const letterBoxes: HTMLDivElement[] = Array.from(document.querySelectorAll(".letter-box"));
 const submitLettersButton = document.querySelector(".js-submit-letters-button") as HTMLButtonElement;
 const resetButton = document.querySelector(".js-reset-button") as HTMLButtonElement;
 
-letter1Box.addEventListener("click", () => addChangeColorOnClick(letter1Box));
-letter2Box.addEventListener("click", () => addChangeColorOnClick(letter2Box));
-letter3Box.addEventListener("click", () => addChangeColorOnClick(letter3Box));
-letter4Box.addEventListener("click", () => addChangeColorOnClick(letter4Box));
-letter5Box.addEventListener("click", () => addChangeColorOnClick(letter5Box));
+
+for(let i = 0; i < 30; i++){
+    letterBoxes[i]!.addEventListener("click", () => addChangeColorOnClick(letterBoxes[i]!, i));
+}
 submitLettersButton.addEventListener("click", () =>{
+    if(isGameWon()){
+        window.alert("Spiel gewonnen!");
+        resetGame();
+        return;
+    }
     addLettersFromBoxes(); //Adds Letters from letterBox1-5 to the appropriate list in solver
-    addBestWord();         //Adds the new best word to the fields and sets their color to undefined
+
+    try{
+        addBestWord(currentRow+1);         //Adds the new best word to the fields and sets their color to undefined
+        currentRow++;
+    }
+    catch(err){
+        window.alert("Word not found, game reset");
+        resetGame();
+    }
 });
 resetButton.addEventListener("click", () =>{
-    solver = new WordleSolver(); //Creates new Instance of solver deleting inputs so far
-    addBestWord();               //Adds the "starting-best-word" = arose to box
+    resetGame();
 });
 
-function addChangeColorOnClick(letterBox: HTMLDivElement) :void{
+function addChangeColorOnClick(letterBox: HTMLDivElement, textBoxNumber: number) :void{
+    if(textBoxNumber >= currentRow*5 && textBoxNumber <= (currentRow+1)*5 -1){
         if(letterBox.classList.contains("grey-letter")){
-        letterBox.classList.toggle("grey-letter");
-        letterBox.classList.toggle("orange-letter");
-        return;
+            letterBox.classList.toggle("grey-letter");
+            letterBox.classList.toggle("orange-letter");
+            return;
+        }
+        if(letterBox.classList.contains("orange-letter")){
+            letterBox.classList.toggle("orange-letter");
+            letterBox.classList.toggle("green-letter");
+            return;
+        }
+        else if(letterBox.classList.contains("green-letter")){
+            letterBox.classList.toggle("green-letter");
+            letterBox.classList.toggle("grey-letter");
+            return;
+        }
+        else{//is still undefined
+            letterBox.classList.toggle("grey-letter");
+        }
     }
-    if(letterBox.classList.contains("orange-letter")){
-        letterBox.classList.toggle("orange-letter");
-        letterBox.classList.toggle("green-letter");
-        return;
-    }
-    else if(letterBox.classList.contains("green-letter")){
-        letterBox.classList.toggle("green-letter");
-        letterBox.classList.toggle("grey-letter");
-        return;
-    }
-    else{//is still undefined
-        letterBox.classList.toggle("grey-letter");
-    }
+
 }
 
-
-
 function clearBoxes(){
-    letter1Box.innerHTML = "";
-    letter2Box.innerHTML = "";
-    letter3Box.innerHTML = "";
-    letter4Box.innerHTML = "";
-    letter5Box.innerHTML = "";
+    for(const curLetterBox of letterBoxes){
+        curLetterBox.innerHTML = "";
+    }
+    removeTypeOfBoxes(0,30);
 }
 
 function addLettersFromBoxes():void {
-    const letterBoxes: HTMLDivElement[] = [letter1Box, letter2Box, letter3Box, letter4Box, letter5Box];
+    const rowLetterBoxes: HTMLDivElement[] = letterBoxes.slice(currentRow*5, (currentRow+1)*5);
     let i: number = 0;//Index of current Box
-    for(const currLetterBox of letterBoxes){
+    for(const currLetterBox of rowLetterBoxes){
         let currChar: string = currLetterBox.innerHTML;
-        if(!solver.isValidChar(currChar)){
-            clearBoxes();
-            return;
-        }
         if(currLetterBox.classList.contains("green-letter")){
             solver.addGreenLetter(i,currChar);
         }
@@ -75,7 +79,7 @@ function addLettersFromBoxes():void {
             //Es kann sein, dass in der gleichen Eingabe ein Buchstabe einmal grün und einmal grau vorkommt, dann muss die graue Stelle nicht über solver.addOrangeLetter
             //an der entsprechenden Stelle hinzugefügt werden
             let j: number = 0; //index of letterBox
-            for(const curLetterBox of letterBoxes){
+            for(const curLetterBox of rowLetterBoxes){
                 if(curLetterBox.innerHTML == currChar && curLetterBox.classList.contains("green-letter") && curLetterBox != currLetterBox){
                     solver.addOrangeLetter(i, currChar);
                     return;
@@ -88,29 +92,44 @@ function addLettersFromBoxes():void {
     }
 }
 
-function addBestWord(): void{
+function addBestWord(row: number): void{//Adds best word in row row
     //Check ob das Ergebnis wirklich nur 5 chars hat, evtl erklärung zu eliminierung der Kandidaten
     let bestWord: string = solver.findBestWord();
     if(bestWord.length != 5){//word wasnt found, resets game
-        window.alert("Word not found, game reset");
-        solver = new WordleSolver();
-        addBestWord();
+        throw new Error("Word not found");
     }
     else{
-        letter1Box.innerHTML = bestWord.charAt(0).toUpperCase();
-        letter2Box.innerHTML = bestWord.charAt(1).toUpperCase();
-        letter3Box.innerHTML = bestWord.charAt(2).toUpperCase();
-        letter4Box.innerHTML = bestWord.charAt(3).toUpperCase();
-        letter5Box.innerHTML = bestWord.charAt(4).toUpperCase();
-        removeTypeOfBoxes(); //Resets all letter types to undefined (White)
+        letterBoxes[row*5]!.innerHTML = bestWord.charAt(0).toUpperCase();
+        letterBoxes[row*5+1]!.innerHTML = bestWord.charAt(1).toUpperCase();
+        letterBoxes[row*5+2]!.innerHTML = bestWord.charAt(2).toUpperCase();
+        letterBoxes[row*5+3]!.innerHTML = bestWord.charAt(3).toUpperCase();
+        letterBoxes[row*5+4]!.innerHTML = bestWord.charAt(4).toUpperCase();
+        removeTypeOfBoxes((row+1)*5,(row+2)*5); //Resets all letter types to undefined (White) in the new row
     }
 }
 
-function removeTypeOfBoxes(): void{
-    const letterBoxes: HTMLDivElement[] = [letter1Box, letter2Box, letter3Box, letter4Box, letter5Box];
-    for(const currLetterBox of letterBoxes){
+function removeTypeOfBoxes(startingIndex:number, endingIndex:number): void{
+   const removalLetterBoxes: HTMLDivElement[] = letterBoxes.slice(startingIndex, endingIndex);
+    for(const currLetterBox of removalLetterBoxes){
         currLetterBox.classList.remove("green-letter");
         currLetterBox.classList.remove("grey-letter");
         currLetterBox.classList.remove("orange-letter");
     }
+}
+
+function resetGame(): void{
+    solver = new WordleSolver();
+    clearBoxes();//clearsletters and types from boxes
+    currentRow = 0;
+    addBestWord(currentRow);
+}
+
+function isGameWon():boolean{
+    const rowLetterBoxes: HTMLDivElement[] = letterBoxes.slice(currentRow*5, (currentRow+1)*5);
+    for(const currLetterBox of rowLetterBoxes){
+        if(!currLetterBox.classList.contains("green-letter")){
+            return false;
+        }
+    }
+    return true;
 }
